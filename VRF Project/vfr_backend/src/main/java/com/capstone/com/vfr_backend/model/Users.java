c3a4e104438f68c4a1fdd6107d234490f62195f1
Ahmad Zain <az682833@gmail.com>
2025-08-24 00:32:33 +0530

    stater validation version fix

--- VRF Project/vfr_backend/src/main/java/com/capstone/com/vfr_backend/model/Users.java
+++ VRF Project/vfr_backend/src/main/java/com/capstone/com/vfr_backend/model/Users.java
@@ -1,11 +1,24 @@
 package com.capstone.com.vfr_backend.model;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.capstone.com.vfr_backend.model.UType.UserType;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,16 +27,36 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long UserId;
+
+    @NotBlank(message = "Name cannot be empty")
+    @Column(nullable=false, length=100)
     private String username;
+
+    @NotBlank(message = "Email cannot be empty")
+    @Column(unique = true, nullable=false, length=100)
+    @Email
+    private String email;
+
+    @Column(nullable = false)
+    @NotNull
+    @Size(min = 8)
     private String password;
-    private String role; // e.g., "USER" or "ADMIN"
+
+    @Enumerated(EnumType.STRING)
+    private UserType role; // e.g., "USER" or "ADMIN"
+
+    @CreationTimestamp
+    @Column(nullable=false, updatable=false)
     private LocalDateTime createdAt;
     
-
+    @OneToMany(mappedBy = "users")
+    private List<Feedback> feedback;
     //need to add validation using stater validation
+
+    
 }
 
--- VRF Project/vfr_backend/src/main/java/com/capstone/com/vfr_backend/model/VASPack.java
+++ VRF Project/vfr_backend/src/main/java/com/capstone/com/vfr_backend/model/VASPack.java
@@ -1,8 +1,16 @@
 package com.capstone.com.vfr_backend.model;
 
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,10 +19,20 @@ import lombok.Setter;
 @Setter
 public class VASPack {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long PackId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pack_id")
+    private Long packId;
 
-    private String PackTitle;
-
+    @NotBlank(message = "Pack title is required")
+    @Size(min = 3, max = 100, message = "Title must be between 3 and 100 characters")
+    @Column(name = "pack_title", nullable = false, length = 100)
+    private String packTitle;
+    
+    @NotBlank(message = "Description is required")
+    @Size(min = 10, max = 500, message = "Description must be between 10 and 500 characters")
+    @Column(name = "description", nullable = false, columnDefinition = "TEXT")
     private String description;
+
+    @OneToMany(mappedBy="vasPack")
+    private List<Feedback> feedbacks;
 }
